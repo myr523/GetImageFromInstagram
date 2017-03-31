cfd5f26de8464b7239c31a79905b35616fc9910c
Ryusei <is0323hi@ed.ritsumei.ac.jp>
2017-03-31 11:03:50 +0900

    仕様変更に対応/エラー処理の拡充

--- getImageFromInsta.php
+++ getImageFromInsta.php
@@ -1,11 +1,8 @@
+#!/bin/php
 <?php
-//hnk -> 1426222753
-//myr -> 1651805211
-date_default_timezone_set('Asia/Tokyo');
-
+define("SAVE_DIR","/Users/myr523/Pictures/MYR/instagram/");
 class GetImageFromInsta{
   public $json_data;
-
   public function __construct($input){
     error_reporting(E_ALL & ~E_NOTICE); //stop warning notice
     $json_data = file_get_contents($input);
@@ -17,33 +14,38 @@ class GetImageFromInsta{
     $this->json_data = $array;  //JSONデータ
     //var_dump($this->json_data);
   }
-
   public function getURLandImage(){
     //動画だった場合の処理
     if ($this->json_data['head']['meta']['22']['@attributes']['content'] == "video") {
-      $videoURL = $this->json_data['head']['meta']['23']['@attributes']['content'];
+      $videoURL = $this->json_data['head']['meta']['24']['@attributes']['content'];
       $vidFilename = basename($videoURL);
-
       $viddata = file_get_contents($videoURL);
-      file_put_contents("/home/myr523/Videos/instagram/{$vidFilename}",$viddata, FILE_APPEND | LOCK_EX);	//保存ディレクトリ指定
-      echo "video successfully saved /home/myr523/Videos/instagram/{$vidFilename}\n";
+      $result = file_put_contents(SAVE_DIR."{$vidFilename}",$viddata, FILE_APPEND | LOCK_EX);
+      if ($result !== false) {
+        echo "video successfully saved.\n";
+      } else {
+        echo "Error was occured.\n";
+      }
     }
-
     //写真・サムネイル取得
-    $url = parse_url($this->json_data['head']['meta']['10']['@attributes']['content']);
+    $url = parse_url($this->json_data['head']['meta']['11']['@attributes']['content']);
     $url = str_replace('/t51.2885-15/e35/', '', $url['path']);
     $picURL = "https://scontent.cdninstagram.com/t51.2885-15/s1080x1080/sh0.08/e35/{$url}";
-
     $picFilename = basename($picURL);
-
     $picdata = file_get_contents($picURL);
-    file_put_contents("/home/myr523/Pictures/instagram/{$picFilename}",$picdata, FILE_APPEND | LOCK_EX);	//保存ディレクトリ指定
-    echo "image successfully saved /home/myr523/Pictures/instagram/{$picFilename}\n";
-    echo "Done.";
+    $result = file_put_contents(SAVE_DIR."{$picFilename}",$picdata, FILE_APPEND | LOCK_EX);
+    if ($result !== false) {
+      echo "image successfully saved.\n";
+    } else {
+      echo "Error was occured.\n";
+    }
   }
 }
-
+if ($argv[1] == NULL) {
+  echo "input args.\n";
+  echo "exit.\n";
+  exit(1);
+}
 $get_image = new GetImageFromInsta($argv[1]);
 $get_image->getURLandImage();
-
 ?>
